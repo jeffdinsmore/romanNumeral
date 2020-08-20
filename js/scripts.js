d7df1e3b4317223a561f53a7916f588f22455734
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-20 16:14:59 -0700

    updated UI elements to handle inputs and offer an output. Updated js with six more constants and a document ready and submit function.
    Co-authored-by: Brycen Bartolome <brycenbartolome@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,44 @@
-const I = "1";
-const V = "5";
-const X = "10";
-const L = "50";
-const C = "100";
-const D = "500";
-const M = "1,000";
-const userInput = (D)
+$(document).ready(function() {
+  $("#form").submit(function(event){
+    const I = "1";
+    const IV = "4";
+    const V = "5";
+    const IX = "9";
+    const X = "10";
+    const XL = "40";
+    const L = "50";
+    const XC = "90";
+    const C = "100";
+    const CD = "400";
+    const D = "500";
+    const CM = "900";
+    const M = "1000";
 
-let output;
-if (userInput === I) {
-	output = "I";
-} else if (userInput === V) {
-	output = "V";
-} else if (userInput === X) {
-	output = "X";
-} else if (userInput === L) {
-	output = "L";
-} else if (userInput === C) {
-	output = "C";
-} else if (userInput === D) {
-	output = "D";
-} else if (userInput === M) {
-	output = "M";
-} else {
-  if 
-}
-console.log(output);
+    const userInput = $("#input").val();
+    console.log(userInput);
+
+    
+
+    let output;
+    if (userInput === I) {
+      output = "I";
+    } else if (userInput === V) {
+      output = "V";
+    } else if (userInput === X) {
+      output = "X";
+    } else if (userInput === L) {
+      output = "L";
+    } else if (userInput === C) {
+      output = "C";
+    } else if (userInput === D) {
+      output = "D";
+    } else if (userInput === M) {
+      output = "M";
+    } 
+    console.log(output);
+    $("#result").text(output);
+    
+    
+    event.preventDefault();
+  });
+});
